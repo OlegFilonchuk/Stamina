632ea3f0a5807219186d66a16d705285d1e49650
Oleg Filonchuk <oifilonchuk@gmail.com>
2019-08-20 13:02:27 +0300

    add signal of the text end

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -7,33 +7,48 @@ export default class TextInput extends Component {
 	state = {
 		str: text1,
 		chars: 0,
+		errors: 0
 	}
 
 	inputRef = React.createRef()
 
 	handleKeyPress = ev => {
-		const { str, chars } = this.state
+		const { str, chars, errors } = this.state
 
-		if (chars === str.length) return
+		if (chars === str.length) {
+			return
+		}
 
 		const offset = (chars + 1) * 12.5
 		
-		if (ev.key !== str[chars]) return 
+		if (ev.key !== str[chars]) {
+			this.setState((prevState) => ({
+				errors: prevState.errors + 1
+			}))
+			console.log('wrong!')
+			return
+		} 
 
+		//move caret
 		ev.target.style.transform = `translateX(-${offset}px)`
 
 		this.setState((prevState) => ({
 			chars: prevState.chars + 1
 		}))
-	}
 
+		//check end of text
+		if (chars === str.length-1) {
+			console.log(`You made ${errors} mistakes`)
+		}
+	}
+	
 	componentDidMount() {
 		this.inputRef.current.focus()
 		document.addEventListener('click', () => this.inputRef.current.focus())
 	}
 
 	restart = () => {
-		this.setState({chars: 0})
+		this.setState({chars: 0, errors: 0})
 		this.inputRef.current.style.transform = 'translateX(0)'
 		this.inputRef.current.focus()
 	}
