02bb35d65c65579815e57fb3a2677a3850b2201f
Oleg Filonchuk <oifilonchuk@gmail.com>
2019-08-20 08:51:16 +0300

    add onkeypress handler

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -4,14 +4,29 @@ import './index.css'
 
 export default class TextInput extends Component {
 
-	handleClick = (ev) => {
-		ev.target.style.transform = `translateX(20px)`
+	state = {
+		str: text1,
+		chars: 0
+	}
+
+	inputRef = React.createRef()
+
+	handleKeyPress = (ev) => {
+		const offset = this.state.chars * 12.5;
+		ev.target.style.transform = `translateX(-${offset}px)`
+		this.setState((prevState) => ({
+			chars: prevState.chars + 1
+		}))
+	}
+
+	componentDidMount() {
+		this.inputRef.current.focus()
 	}
 
 	render() {
 		return (
 			<div className="text-input-container" ref={this.ref}>
-				<div className="text-input" onClick={this.handleClick}>
+				<div className="text-input" onKeyPress={this.handleKeyPress} tabIndex="-1" ref={this.inputRef}>
 					{text1}
 				</div>
 			</div>
