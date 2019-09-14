92c33cbb6cc1ae23c64cd8e5a87288c20eb1cf7b
Andrey Rechets <arechec@freshcode.org>
2019-09-14 13:48:52 +0300

    Server start

---
+++ server/src/app.ts
@@ -0,0 +1,19 @@
+import * as express from 'express';
+import * as cors from 'cors';
+
+export class App {
+
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+    }
+
+    private config() {
+        this.app.use('/public', express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+}
---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import {App} from './app';
+
+const app = new App().app;
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+   console.log(`Server started on port ${PORT}`);
+});
